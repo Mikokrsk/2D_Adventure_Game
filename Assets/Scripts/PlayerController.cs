26e3d7aaccc0c849cc963704ef240232ea2c4c19
Mykola Krasko <mikokrsk@gmail.com>
2024-01-02 21:40:30 +0200

    Game environment and physics
    + Created a tilemap game environment.
    = Decorated environment with sprites.
    + Implement collision between the player character and the game world.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] public InputAction MoveAction;
+    [SerializeField] private Vector2 move;
+    [SerializeField] private Rigidbody2D rigidbody2d;
+    [SerializeField] public int maxHealth = 5;
+    [SerializeField] public float speed = 3.0f;
+    [SerializeField] public int currentHealth;
+    public int health { get { return currentHealth; } }
     // Start is called before the first frame update
     void Start()
     {
-
+        currentHealth = maxHealth;
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
     }
 }
